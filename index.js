733835f658c64609bbbc4ea22cc2d5cef0cc6754
libaice <libaice147@gmail.com>
2022-07-28 14:10:28 +0800

    add interact with wallet and add ethers framework

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-console.log("hello baice ~"); 
-async function connect(){
-    if(typeof window.ethereum !== "undefined" ){
-    console.log(" I see a metamask !");
+// In front end project cannot use require
 
-    window.ethereum.request({method:"eth_requestAccounts"})
-    
-    document.getElementById("connectButton").innerHTML = "Connected !"
+import {ethers} from "./ethers-5.6.esm.min.js";
+import {abi, contractAddress} from "./constant.js";
 
-}else{
-    console.log(" i cannot find metamask in your browser");
-} 
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fund")
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    console.log("HELLO , conncet wallet")
+    if (typeof window.ethereum !== "undefined") {
+        console.log(" I see a metamask !");
+        await window.ethereum.request({method: "eth_requestAccounts"})
+        // document.getElementById("connectButton").innerHTML = "Connected !"
+        connectButton.innerHTML = "Connected  !"
+    } else {
+        console.log(" i cannot find metamask in your browser");
+    }
 }
 
 
+async function fund() {
+    const ethAmount = "50"
+    console.log(`Funding with ${ethAmount} ...  `)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner();
+
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        const txResponse = await contract.fund({
+            value: ethers.utils.parseEther(ethAmount)
+        })
+    }
+
+
+}
 
 // fund function
 
